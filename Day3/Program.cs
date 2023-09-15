f99101ec0d00e4901be7fc749233c0f9604eab91
Emirhan Dilber <edilberfb@gmail.com>
2023-09-15 14:36:32 +0300

    Day3 solutin

---
+++ Day3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        if(n % 2 != 0)
+            Console.WriteLine("Weird");
+        else
+            if(2<=n && n<=5)
+                Console.WriteLine("Not Weird");
+            else if(6 <= n && n<=20)
+                Console.WriteLine("Weird");
+            else
+                Console.WriteLine("Not Weird");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HackerRank-Csharp-Exercises
 I'm going to solve the exercises and pull solutions below this repo
+If you don't understand the soluiton, please text me :)
